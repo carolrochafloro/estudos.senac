7795f47ced456c58899dbd31a06fe2fbbca39a9f
carolrochafloro <carolinarochaf@hotmail.com>
2023-04-29 15:28:46 -0300

    programa que conta os números pares em C#

---
+++ soma-numeros-pares/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    public static void Main (string[] args)
+    {
+        int num = -1; //inicializa com um número negativo para entrar no loop
+        int soma = 0;
+        int tentativas = 3; //número máximo de tentativas
+
+        while  (num < 0 && tentativas > 0) {
+            Console.WriteLine("Digite um número:");
+            num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("O número deve ser positivo.");
+                tentativas--;
+                Console.WriteLine("Tentativas restantes: {0}", tentativas);
+            }
+        }
+           
+
+        if (tentativas == 0) //todas as tentativas foram usadas
+        {
+            Console.WriteLine("Todas as tentativas foram usadas.");
+            return;
+        }
+
+        else //foi digitado um número positivo
+        {
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    soma += i;
+                }
+            }
+            Console.WriteLine("A soma dos números pares até {0} é {1}", num, soma);
+        }
+    }
+        
+
+}
