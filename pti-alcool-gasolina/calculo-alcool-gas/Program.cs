0f0c42fe1ed3e7d7eb716e01b3343cfaa645d731
carolrochafloro <carolinarochaf@hotmail.com>
2023-04-30 10:32:22 -0300

    calculo alcool x gasolina

---
+++ pti-alcool-gasolina/calculo-alcool-gas/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    double gas, alcool, result;
+    bool novoCalculo = true;
+    string resposta;
+
+    do 
+    { 
+    Console.WriteLine("Digite o preço do álcool:");
+    alcool = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o preço da gasolina:");
+    gas = double.Parse(Console.ReadLine());
+
+    result = (alcool/gas);
+
+    if (result > 0.7) 
+    {
+      Console.WriteLine("Abasteça com gasolina");
+    }
+
+    else if (result < 0.7) 
+    {
+      Console.WriteLine("Abasteça com álcool.");
+    }
+
+    else 
+    {
+      Console.WriteLine("Tanto faz");
+    }
+
+    Console.WriteLine("Pressione <N> para calcular novamente ou <S> para sair");
+    resposta = Console.ReadLine();
+
+    if (resposta.ToUpper() != "N") 
+    {
+      novoCalculo = false;
+    }
+    }
+    while (novoCalculo);
+
+  }
+  }
