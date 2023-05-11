480ff5ffe209bd4c7ea4d6713bc4f05d1b93f7ea
carolrochafloro <carolinarochaf@hotmail.com>
2023-05-11 18:25:44 -0300

    atualizacao

---
+++ autenticacao/Autenticacao/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    string nome;
+    string senha;
+
+    Console.WriteLine("Nome de usuário:");
+    nome = Console.ReadLine();
+
+    Console.WriteLine("Senha");
+    senha = Console.ReadLine();
+    
+// && = operador lógico E
+    if (nome == "Maria" && senha == "1234"){
+      for (int i=0; i<=10; i++) {
+        if(i % 2 == 0){
+          Console.WriteLine(i);
+        }
+      }
+    }
+    else {
+      Console.WriteLine("Acesso negado");
+    }
+
+    Console.ReadKey();
+
+  }
+}
---
+++ autenticacao/inicioprojetocs.md
@@ -0,0 +1,11 @@
+# Como iniciar um projeto em C# no VS Code
+
+1- Criar pasta
+2- Criar solução
+3- Botão direito + add new project -> Aplicativo de Console
+4- P/ depurar
+4.1 Run and debug -> generate C# assets
+4.1.1 Se não gerar os arquivos corretos, ir em Solution -> botão direito -> Clean
+4.2 Launch.json -> mudar "console" para "externalTerminal"
+
+#### Fonte: https://www.youtube.com/watch?v=YEtisxQugew&ab_channel=FaculdadeVincit
