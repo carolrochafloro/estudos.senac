f17b78de137b09bdd141dce2d39b1b92636f9a43
carolrochafloro <carolinarochaf@hotmail.com>
2023-07-03 20:43:18 -0300

    estruturas repetitivas

---
+++ csharp-nelioalves/EstCondicional2/EstCondicional2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace EstCondicional2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for - quando você sabe a quantidade de repetições
+            //for (inicio (só na primeira vez, voltando não roda de novo); condicao (v - executa e volta, f - pula);
+            //incremento(executa toda vez depois de voltar)) {
+            //comando
+            //} 
+
+            int x, y, min, max, soma;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else
+            {
+                min = y;
+                max = x;
+            }
+
+            soma = 0;
+
+            for (int i = min + 1; i < max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+
+            Console.WriteLine(soma);
+
+        } //do-while: o bloco de comandos é executado pelo menos 1x, pois a condição é verificada no final.
+        // do { comando} while (condição);
+    }
+}
+
+
+
+
+    
---
+++ csharp-nelioalves/EstRepetitivas/EstRepetitivas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstRepetitivas
+
+//while: quando não souber o número de repetições. Repete enquanto uma condição for verdadeira.
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (condição) {comando a ser repetido. v = executa e volta pra condição. f = pula.}
+            int opcao = int.Parse(Console.ReadLine());
+            int totalAlcool = 0;
+            int totalGasolina = 0;
+            int totalDiesel = 0;
+
+
+            while (opcao != 4)
+            {
+                if (opcao > 4 || opcao < 0)
+                {
+                    opcao = int.Parse(Console.ReadLine());
+                } else if (opcao == 1) {
+                    totalAlcool = totalAlcool + 1;
+                    opcao = int.Parse(Console.ReadLine());
+                } else if (opcao == 2) {
+                    totalGasolina = totalGasolina + 1;
+                    opcao = int.Parse(Console.ReadLine());
+                } else
+                {
+                    totalDiesel = totalDiesel + 1;
+                    opcao = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + totalAlcool);
+            Console.WriteLine("Gasolina: " + totalGasolina);
+            Console.WriteLine("Diesel: " + totalDiesel);
+
+        }
+    }
+}
