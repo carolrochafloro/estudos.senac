3d05ad469cd3b46f850dfed7143fe42a4ff2a332
carolrochafloro <carolinarochaf@hotmail.com>
2023-07-06 08:57:12 -0300

    Prática repetitivas e condicionais Beecrowd

---
+++ csharp-nelioalves/Pratica/Pratica/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Pratica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, m;
+            int max, min;
+            int soma = 0;
+
+            do
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                m = int.Parse(vet[0]);
+                n = int.Parse(vet[1]);
+                soma = 0;
+
+                if (m > 0 && n > 0)
+                {
+                    if (m < n)
+                    {
+                        max = n;
+                        min = m;
+                    }
+                    else
+                    {
+                        max = m;
+                        min = n;
+                    }
+
+                    for (int i = min; i <= max; i++)
+                    {
+                        Console.Write(i + " ");
+                        soma = soma + i;
+                    }
+                    Console.WriteLine("Sum=" + soma);
+                }
+
+            } while (m > 0 && n > 0);
+        }
+    }
+}
+
+
+
+
+
+
+
+
