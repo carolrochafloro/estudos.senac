7127f8101dc3ba3171dbf8669195aa5a72c1b306
carolrochafloro <carolinarochaf@hotmail.com>
2023-07-10 20:18:20 -0300

    Prática est. de repetição e algoritmo

---
+++ csharp-nelioalves/Pratica2/Pratica2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pratica2NaoSubir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int contador = 1;
+
+            int x = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int soma = x;
+            int proximo = x + 1;
+
+            while (n <= x)
+            {
+                n = int.Parse(Console.ReadLine());
+
+            }
+
+            while (soma <= n)
+            {
+                contador++;
+
+                soma = soma + proximo;
+
+                proximo = proximo + 1;
+
+            }
+
+
+
+            Console.WriteLine(contador);
+
+        }
+    }
+}
